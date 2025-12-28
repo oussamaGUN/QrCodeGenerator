f0f8ce4b989a2de2358980b3487a2d93ac23af23
oussama sabbar <sebbaroussama3@gmail.com>
2025-12-28 18:32:30 +0100

    add the according components

---
+++ frontend/app/components/configuration.tsx
@@ -0,0 +1,59 @@
+'use client'
+import React from "react"
+import { Box } from "@mui/material"
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ConfigurationContent from "./configurationContent/configurationContent";
+
+export default function ConfigurationComponent({configuration}: {configuration: any}) {
+  return (
+    <Box sx={{
+      mt: '1rem',
+      width: '100%',
+    }}>
+      <Accordion defaultExpanded>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+          sx={{
+            bgcolor: 'var(--border)',
+            color: 'var(--muted-text)',
+            '& .MuiAccordionSummary-expandIconWrapper': {
+              color: 'var(--muted-text)',
+            },
+
+            '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+              color: 'var(--primary)',
+            },
+          }}
+        >
+          <Typography component='span' sx={{color: 'var(--primary)', mr: '.5rem'}}>{configuration.icon}</Typography>
+          <Typography component="span" sx={{textTransform: 'uppercase', fontWeight: '600'}}>
+            {configuration.title} Configuration
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <ConfigurationContent configuration={configuration}/>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <Typography component="span">Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+
+  )
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,8 +3,12 @@ import { Box, Button, Typography, Grid } from "@mui/material";
 import HeaderText from "./components/header_text";
 import SelectActionCard from "./components/cards";
 import Preview from "./components/preview";
+import React from "react";
+import ConfigurationComponent from "./components/configuration";
+import LinkIcon from '@mui/icons-material/Link';
 
 export default function Home() {
+  const [configuration, setConfiguration] = React.useState<any>({id: 1, icon: <LinkIcon />,title: 'Url'});
   return (
     <div className="bg-[var(--background)] ">
       <Box
@@ -17,14 +21,8 @@ export default function Home() {
           <Grid size={8} sx={{
             pt: '2rem'
           }}>
-            <SelectActionCard />
-              <Box sx={{
-                mt: '1rem',
-                bgcolor: 'violet',
-                width: '100%',
-                height: '60vh'
-              }}>
-              </Box>
+            <SelectActionCard setConfiguration={setConfiguration}/>
+            <ConfigurationComponent configuration={configuration}/> 
           </Grid>
           <Grid size={4}>
             <Preview />
