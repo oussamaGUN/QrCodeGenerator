bcb7e1e08faa33f9a3fd3f26f4d2c0681480a746
oussama sabbar <sebbaroussama3@gmail.com>
2025-12-28 15:34:44 +0100

    frontend file

---
+++ frontend/app/components/preview.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+export default function Preview(){
+  return (
+    <>
+      <Box sx={{
+        mt: '2rem',
+        bgcolor: 'violet',
+        width: '100%',
+        height: '70vh',
+        position: 'sticky',
+        top: 30,
+      }}>
+      </Box>
+    </>
+  )
+}
